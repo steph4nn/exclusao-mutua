32a69d1326b8e0eff22c546dfc169c63f9a84040
steph4nn <laurostephan@gmail.com>
2023-11-13 12:26:37 -0300

    #

---
+++ main.py
@@ -0,0 +1,36 @@
+
+import threading
+import time
+from threading import Semaphore
+
+numero = 0
+mutex = Semaphore(1)
+
+
+# Codigo estah pulando numeros, e repetindo numeros entre threads
+
+def p1():
+    global numero
+    while True:
+        global mutex    
+        mutex.acquire()
+        numero += 1
+        mutex.release()
+        print('P1:', numero)
+
+def p2():
+    global numero
+    while True:
+        global mutex
+        mutex.acquire()
+        numero += 1
+        print('P2:', numero)
+        mutex.release()
+
+
+t_p1 = threading.Thread(target=p1)
+t_p2 = threading.Thread(target=p2)
+
+t_p1.start()
+t_p2.start()
+
